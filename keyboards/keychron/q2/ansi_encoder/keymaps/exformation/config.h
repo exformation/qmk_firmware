339efdd287e481fd4935d26ead821dfee499f5ee
exform <mmillwood256@gmail.com>
2024-12-10 12:44:50 -0600

    boop

--- keyboards/keychron/q2/ansi_encoder/keymaps/exformation/config.h
+++ keyboards/keychron/q2/ansi_encoder/keymaps/exformation/config.h
@@ -1,4 +1,4 @@
 #define TAPPING_TERM 200
 // #define PERMISSIVE_HOLD // Adown Bdown Bup Aup
 #define HOLD_ON_OTHER_KEY_PRESS // if you want rolls->Adown Bdown Aup Bup
-#define TAP_CODE_DELAY 1
+#define TAP_CODE_DELAY 20
